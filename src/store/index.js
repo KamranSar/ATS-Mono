447f19fb3daaefce8f543e4dacdfc69c505fc56b
Nathan Brizzee <nathan.brizzee@cdcr.ca.gov>
2021-03-03 02:45:46 -0800

    Added example of custom control over set/get of keys

--- src/store/index.js
+++ src/store/index.js
@@ -16,10 +16,11 @@ Vue.use(Vuex);
 const vuexCookie = new VuexPersistence({
   key: 'cookieStore', // The key to store the state on in the storage provider.
   restoreState: (key) => Cookies.getJSON(key),
-  saveState: (key, state) =>
-    Cookies.set(key, state, {
+  saveState: (key, state) => {
+    return Cookies.set(key, state, {
       expires: 3, // Number of days to expire cookie in
-    }),
+    });
+  },
   modules: ['authentication'], //only save user module
   // https://github.com/championswimmer/vuex-persist/issues/178
   filter: (mutation) => mutation.type.includes('authentication/'), // Method to filter which mutations will trigger state saving
@@ -33,15 +34,23 @@ const vuexLocal = new VuexPersistence({
   filter: (mutation) =>
     mutation.type.includes('userprefs/') ||
     mutation.type.includes('serviceReq/'), // Method to filter which mutations will trigger state saving
-  //filter: (mutation) => console.log(mutation.type),
-  // mutation.type.includes('serviceReq/') ||
-  // mutation.type.includes('userprefs/'), //modules doesn't filter, so filter the mutations
 });
 
-const vuexAzure = new VuexPersistence({
-  key: 'azureStore', // The key to store the state on in the storage provider.
+const vuexIndexdb = new VuexPersistence({
+  key: 'indexdbStore', // The key to store the state on in the storage provider.
   storage: localForage, // or window.sessionStorage or localForage
   asyncStorage: true,
+  // Define custom set/get for state so we can log what's getting saved/restored
+  restoreState: (key) => {
+    // console.log(key + '_restored');
+    return localForage.getItem(key);
+  },
+  saveState: (key, state) => {
+    // console.log(key + '_saved');
+    // console.log(state);
+    return localForage.setItem(key, state);
+  },
+
   modules: ['azureAuthentication'],
   // reducer: (state) => ({ azureAuthentication: state.azureAuthentication }),
   filter: (mutation) => mutation.type.includes('azureAuthentication/'), // Method to filter which mutations will trigger state saving
@@ -60,7 +69,7 @@ const store = new Vuex.Store({
     pathify.plugin,
     vuexCookie.plugin,
     vuexLocal.plugin,
-    vuexAzure.plugin,
+    vuexIndexdb.plugin,
   ],
 });
 
