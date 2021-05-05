a469d79369dc4c93a29504042c4a1f62ac939d52
Nurthin Aziz <NURTHIN.AZIZ@cdcr.ca.gov>
2021-05-04 18:27:58 -0700

    crypto for localStorage - Does not seperate keys by module name in LS

--- src/store/plugins/localStorage.js
+++ src/store/plugins/localStorage.js
@@ -1,10 +1,44 @@
 import VuexPersistence from '@/../local_modules/vuex-persist';
+import {
+  knownToken,
+  encryptData,
+  decryptData,
+} from '@/store/plugins/crypto.js';
 
-const modules = ['userprefs', 'users']; // Modules you want to save to persistence
-
+const modules = ['userprefs', 'Users']; // Modules you want to save to persistence
+const storageKey = `ls.${process.env.VUE_APP_NAME}`;
+const PROD_ENV = process.env.NODE_ENV === 'production';
 const vuexPersist = new VuexPersistence({
-  key: `ls.${process.env.VUE_APP_NAME}`, // The key to store the state on in the storage provider.
-  storage: window.localStorage, // or window.sessionStorage, window.localStorage, or localForage
+  storage: {
+    getItem: async () => {
+      // Get the store from local storage.
+      let data = window.localStorage.getItem(storageKey);
+      if (data && PROD_ENV) {
+        try {
+          // Decrypt data retrieved from local storage
+          return await decryptData(data, knownToken.a, knownToken.b);
+        } catch (e) {
+          // FIXME: Do we want this?
+          // Data will be reset if decryption fails.
+          window.localStorage.removeItem(storageKey);
+        }
+      } else {
+        // Not running in production, return unecrypted data
+        return data;
+      }
+      return null;
+    },
+    setItem: async (key, value) => {
+      let data = value;
+      if (PROD_ENV) {
+        // Encrypt the data in production
+        data = await encryptData(value, knownToken.a, knownToken.b);
+      }
+
+      // Save the data in local storage.
+      return window.localStorage.setItem(storageKey, data);
+    },
+  },
   modules: [...modules],
   // https://github.com/championswimmer/vuex-persist/issues/178
   filter: (mutation) => {
