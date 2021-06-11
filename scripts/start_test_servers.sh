1942c2328a473add7ab1c2da793598fb024ea531
Kevin Baroni <kevin.baroni@cdcr.ca.gov>
2021-06-11 10:50:13 -0700

    Encrypted Oracle creds.

--- scripts/start_test_servers.sh
+++ scripts/start_test_servers.sh
@@ -156,8 +156,8 @@ else
   --env APP_PORT=${MT_EIS_COMMON_PORT} \
   --env ORACLE_SVR_PORT=1521 \
   --env ORACLE_GUI_PORT=5500 \
-  --env ORACLE_USER=interface_user[eomis_ro_user] \
-  --env ORACLE_PASSWORD=USERif##2020 \
+  --env ORACLE_USER=aW50ZXJmYWNlX3VzZXJbZW9taXNfcm9fdXNlcl0= \
+  --env ORACLE_PASSWORD=VVNFUmlmIyMyMDIw \
   --env ORACLE_CONNSTR=vcx1-scan.accounts.cdcr.ca.gov:1521/somstest1.vcx \
   --expose ${MT_EIS_COMMON_PORT} \
   --name $IMAGE_NAME \
