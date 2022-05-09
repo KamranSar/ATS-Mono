a6df0aceacf46c23546bfd694c0ea18f7fa5fe7d
Tim Gaither <Tim.Gaither@cdcr.ca.gov>
2022-05-09 21:43:43 +0000

    Merged PR 1163: Fixed case factors displaying on 135 after saving Endorsement on Schedule page.
    Fixed the saving of the record to the database so the case factors were included. 135 report displays case factors now.
    
    Related work items: #3273

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -399,7 +399,7 @@
     computed: {
       // ...get('institutions', ['listOfInstitutions']),
       ...sync('institutions', ['selectedInstitution', 'listOfInstitutions']),
-      ...sync('transfers', ['transferData']),
+      ...sync('transfers', ['transferData', 'somsOffender']),
       ...sync('schedules', ['schedules', 'selSchedule']),
       ...get('users', ['loggedInUser']),
       ...get('reasons', ['reasons']),
@@ -806,6 +806,10 @@
             this.transferData.endorsedToPartyId = objIns.institutionPartyId;
           }
 
+          this.transferData.caseFactor = this.formatCaseFactors(
+            this.somsOffender
+          );
+
           const response = await this.saveForm();
           if (response && response._id) {
             this.setSnackbar('Success!', 'success', 3000);
@@ -824,6 +828,86 @@
           this.setSnackbar(`Failure!`, 'error', 3000);
           return false;
         }
+      },
+      formatCaseFactors(item) {
+        if (!item && !item.CaseFactors && item.CaseFactors.length === 0) {
+          // item is empty
+          console.log('ERROR! Schedule::formatCaseFactors(): item => ', item);
+          return;
+        }
+        // console.log('formatCaseFactors(): item => ', item);
+        const cf = [];
+        // console.log(
+        //   'formatCaseFactors(): lifer_lwop_flag => ',
+        //   item.CaseFactors[0].lifer_lwop_flag
+        // );
+        if (item.CaseFactors[0].lifer_lwop_flag) {
+          cf.push('sny: ' + item.CaseFactors[0].lifer_lwop_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): sny_flag => ',
+        //   item.CaseFactors[0].sny_flag
+        // );
+        if (item.CaseFactors[0].sny_flag) {
+          cf.push('sny: ' + item.CaseFactors[0].sny_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): item => ',
+        //   item.CaseFactors[0].cccms_eop_flag
+        // );
+        if (item.CaseFactors[0].cccms_eop_flag) {
+          cf.push('ccms-eop: ' + item.CaseFactors[0].cccms_eop_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): item => ',
+        //   item.CaseFactors[0].cocci1_flag
+        // );
+        if (item.CaseFactors[0].cocci1_flag) {
+          cf.push('cocci1: ' + item.CaseFactors[0].cocci1_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): item => ',
+        //   item.CaseFactors[0].cocci2_flag
+        // );
+        if (item.CaseFactors[0].cocci2_flag) {
+          cf.push('cocci2: ' + item.CaseFactors[0].cocci2_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): item => ',
+        //   item.CaseFactors[0].ddp_flag
+        // );
+        if (item.CaseFactors[0].ddp_flag) {
+          cf.push('DDP: ' + item.CaseFactors[0].ddp_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): item => ',
+        //   item.CaseFactors[0].dpp_flag
+        // );
+        if (item.CaseFactors[0].dpp_flag) {
+          cf.push('DPP: ' + item.CaseFactors[0].dpp_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): item => ',
+        //   item.CaseFactors[0].ice_flag
+        // );
+        if (item.CaseFactors[0].ice_flag) {
+          cf.push('ice: ' + item.CaseFactors[0].ice_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): item => ',
+        //   item.CaseFactors[0].retainASU_flag
+        // );
+        if (item.CaseFactors[0].retainASU_flag) {
+          cf.push('Retain ASU: ' + item.CaseFactors[0].retainASU_value);
+        }
+        // console.log(
+        //   'formatCaseFactors(): item => ',
+        //   item.CaseFactors[0].transferMERD_flag
+        // );
+        if (item.CaseFactors[0].transferMERD_flag) {
+          cf.push('MERD: ' + item.CaseFactors[0].transferMERD_value);
+        }
+        return cf.join(', ');
       },
       // getInstitutionId(location)
       // Returns the abbreviated institution id
